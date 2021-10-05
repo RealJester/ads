611e3b9c02ef77281b158ee964c4dc03129b3e1d
Eugene Klim <klimus1002@gmail.com>
2021-10-05 13:02:35 +0300

    REST-ful ads

--- app/controllers/advertisments_controller.rb
+++ app/controllers/advertisments_controller.rb
@@ -1,2 +1,46 @@
 class AdvertismentsController < ApplicationController
+
+
+  def index
+    @advertisments = Advertisment.all
+  end	
+
+  def show
+    @advertisment = Advertisment.find(params[:id])	
+  end	
+
+  def new
+    @advertisment = Advertisment.new
+  end	
+
+  def edit
+    @advertisment = Advertisment.find(params[:id])
+  end	
+
+  def create
+  	@advertisment = Advertisment.new(advertisment_params)
+
+    if @advertisment.save
+      redirect_to @advertisment
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end	
+
+  def update
+    @advertisment = Advertisment.find(params[:id])
+    if @advertisment.update(advertisment_params)
+      redirect_to @advertisment
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end	
+
+  def destroy
+  end
+
+  private
+    def advertisment_params
+      params.require(:advertisment).permit(:title, :body)
+    end
 end
--- app/models/advertisment.rb
+++ app/models/advertisment.rb
@@ -1,2 +1,8 @@
 class Advertisment < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3}
+  validates :body, presence: true
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
 end
