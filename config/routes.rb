590f1c35e44d9978c540f4ba200a06acc83caef5
Eugene Klim <klimus1002@gmail.com>
2021-10-19 15:16:18 +0300

    Listing users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do 
+    get 'users' => 'users#index'
+  end
+  
+  resources :advertisments
   root 'static_pages#home'
-  get 'home'    => 'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  resources :advertisments
 end
